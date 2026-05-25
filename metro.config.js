809d26a2ceb0684604e85481cedb9a4faa6046de
Iulian <iulian.birladeanu@student.upt.ro>
2026-05-25 15:02:38 +0300

    fix: exclude opentelemetry from bundle

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,11 @@ const { getDefaultConfig } = require('expo/metro-config');
 const config = getDefaultConfig(__dirname);
 config.resolver.sourceExts.push('cjs');
 
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (moduleName.includes('@opentelemetry')) {
+    return { type: 'empty' };
+  }
+  return context.resolveRequest(context, moduleName, platform);
+};
+
 module.exports = config;
